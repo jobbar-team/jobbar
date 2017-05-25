b32126846afdc85b4c760ffd88c540158a4a7bed
Marina Tot <tmarina89@gmail.com>
2017-05-26 00:38:10 +0200

    This is little change from first time

--- src/main/webapp/public/js/index.js
+++ src/main/webapp/public/js/index.js
@@ -1,9 +1,14 @@
+	
+
 function toggleMenu() {
-  var menuBox = document.getElementById('menu-box');    
-  if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
-    menuBox.style.display = "none";
-  }
-  else { // if is menuBox hidden, display it
+	var menuBox = document.getElementById('menu-box');
+	var icon_X = document.getElementsByClassName("fa fa-times")[0];
     menuBox.style.display = "block";
+    icon_X.style.display = "block";
   }
-}
+function closeNavBar(){ 
+	var menuBox = document.getElementById('menu-box');
+	var icon_X = document.getElementsByClassName("fa fa-times")[0];
+  	icon_X.style.display = "none";
+  	menuBox.style.display = "none";
+ }
